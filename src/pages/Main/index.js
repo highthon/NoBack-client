d60781ce5a13b00a74790c32b5023e5797824639
sookyoungwoo <sookyoung2005@gmail.com>
2023-01-14 20:30:24 +0900

    ADD ::  화면 css

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,19 +1,28 @@
-import './style.scss';
+import "./style.scss";
 import Header from "../../components/header";
-export default function Main(){
-    return(
-        <>
-            <div className='main-tit'>
-            <Header/>
-                <div className='main-tit-tle'>
-                    <div className='main-tit-ment'>
-                        <p className='ment-first'>당신의</p>
-                        <p className='ment-second'>취향에 맞는</p>
-                        <p className='ment-third'>취미를</p>
-                        <p className='ment-four'>찾아보세요</p>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+export default function Main() {
+  return (
+    <>
+      <div className="main-tit">
+        <Header />
+        <div className="main-tit-tle">
+          <div className="main-tit-ment">
+            <p className="main-tit-ment-1">
+              <img src="/img/main-img1.svg" />
+              <a className="ment-first">당신의</a>
+            </p>
+            <p className="main-tit-ment-2">
+              <a className="ment-second">취향에 맞는</a>
+            </p>
+            <p className="main-tit-ment-3">
+              <img src="/img/main-img3.svg" className="img-3-1" />
+              <a className="ment-third">취미를</a>
+              <img src="/img/main-img2.svg" className="img-3-2" />
+            </p>
+            <a className="ment-four">찾아보세요</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
